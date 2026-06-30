1af4a0ac85af1340740f79cec72f4521f6ad656e
Mark <mark.sahar18@mail.ru>
2026-06-30 13:58:05 +0200

    first commit

---
+++ android/app/src/main/java/com/kids/learning/MainActivity.java
@@ -0,0 +1,5 @@
+package com.kids.learning;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.kids.learning',
+  appName: 'kids-learning',
+  webDir: 'dist'
+};
+
+export default config;
